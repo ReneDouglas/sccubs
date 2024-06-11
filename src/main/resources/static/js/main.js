8075acd08c9ed7d9cc2550c7cd80034b1bdd1fe9
faonictor <jnictoraraujo@gmail.com>
2024-06-11 09:55:54 -0300

    Primeiras Telas

---
+++ src/main/resources/static/js/main.js
@@ -0,0 +1,19 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const headers = document.querySelectorAll('.accordion-header');
+
+    headers.forEach(header => {
+        header.addEventListener('click', function () {
+            const content = this.nextElementSibling;
+            const arrow = header.querySelector('.arrow')
+
+            if (content.style.height && content.style.height !== '0px') {
+                content.style.height = '0';
+                content.style.overflow = 'hidden';
+            } else {
+                content.style.height = content.scrollHeight + 'px';
+                content.style.overflow = 'visible';
+            }
+            arrow.classList.toggle('rotate-180')
+        });
+    });
+});
